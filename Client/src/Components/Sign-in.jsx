b953ac0032a2b5a111ea2ecacb9847d132bfadeb
Emmanuel Mshindi <emmanuelmshindi68@gmail.com>
2025-05-20 13:30:07 +0300

    Update Sign-in.jsx

--- Client/src/Components/Sign-in.jsx
+++ Client/src/Components/Sign-in.jsx
@@ -10,7 +10,7 @@ export default function SignIn({onBackToSignUp}){
     const handleSubmitLoginForm = async (e) => {
         e.preventDefault();
         try {
-        const data = await axios.post('http://localhost:5000/api/login', {
+        const data = await axios.post('https://memorydrop.onrender.com/api/login', {
             email,
             password
         });
